dc7ea7e92347129bea5831dab2d476b57e155d32
zipperr <zipperr@i.softbank.jp>
2018-01-14 23:13:21 +0900

    fix

--- login.py
+++ login.py
@@ -1,28 +1,25 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup #HTML整形ツール
-from requests.excecations import ConnectionError
+from requests.exceptions import ConnectionError
 import requests, sys
 
 class TwitterLogin:
 	def __init__(self):
-		#ログインするユーザ名
-		self.username = "アカウントめい"
-		#ログインユーザのパスワード
-		self.password = "パスワード"
+		self.username = ""
+		self.password = ""
 
-def login(self):
-
+	def login(self):
 		#セッション生成
 		session = requests.Session()
+
 		headers ={
-			"User-Agent":                "Mozilla/5.0",
-			"accept":                    "test/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-			"accept-language":           "ja,en-US;q=0.8,en;q=0.6",
-			"content-type":              "application/x-www-form-urlencoded",
-			"origin":                    "https://twitter.com",
-			"referer":                   "https://twitter.com/",
+			"User-Agent": "Mozilla/5.0",
+			"accept": "test/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+			"accept-language": "ja,en-US;q=0.8,en;q=0.6",
+			"content-type": "application/x-www-form-urlencoded",
+			"origin": "https://twitter.com",
+			"referer": "https://twitter.com/",
 			"upgrade-insecure-requests": "1"
 		}
 
@@ -36,31 +33,31 @@ def login(self):
 			"redirect_after_login": "/", 
 		}
 
-# authenticity_token値の取得
-try:
-	response   = session.get('https://twitter.com/',headers = headers, allow_redirects = False)
-	soup       = Beautifulsoup(response.text,"lxml")
-	auth_token = soup.find(attrs= {'name': 'authenticity_token'}).get('Value')
-except ConnectionError:
-	print("[*]Twitterへ接続できません")
-	sys.exit() 
+		# authenticity_token値の取得
+		try:
+			response   = session.get('https://twitter.com/',headers = headers, allow_redirects = False)
+			soup       = BeautifulSoup(response.text,"lxml")
+			auth_token = soup.find(attrs= {'name': 'authenticity_token'}).get('value')
+		except ConnectionError:
+			print("[*]Twitterへ接続できません")
+			sys.exit()
+		
+			#authenticity_tokenをpayloadに設定
+			payload['authenticity_token'] = auth_token
+			#ユーザーIDをpayloadに設定
+			payload['session[username_or_email]'] = self.username
+			#パスワードをpayloadに設定
+			payload['session[password]'] = self.password
 
-	#authenticity_tokenをpayloadに設定
-	payload['authenticity_token'] = auth_token
-	#ユーザーIDをpayloadに設定
-	payload['session[username_or_email]'] = self.username
-	#パスワードをpayloadに設定
-	payload['session[password]'] = self.password
-
-#twitterにログイン
-try:
-	login =session.post('https://twitter.com/session', headers=headers, data=payload, allow_redirects=False)
-	if login.status_code == 302:
-		print("[+] ログイン完了 HTTPステータスコード: ")
-		print(login.status_code)
-except:
-	print("[+] ログイン中に通信エラー")
+		#twitterにログイン
+		try:
+			login =session.post('https://twitter.com/sessions', headers=headers, data=payload, allow_redirects=False)
+			if login.status_code == 302:
+				print("[+] ログイン完了 HTTPステータスコード: ")
+				print(login.status_code)
+		except:
+			print("[+] ログイン中に通信エラー")
 
 if __name__ == '__main__':
-	TwiLogin = Twitterlogin()
+	TwiLogin = TwitterLogin()
 	TwiLogin.login()
